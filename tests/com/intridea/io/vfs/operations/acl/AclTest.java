c590a5be1010468687fe6a141c5664ca90fbb0ba
alex.kovalyov <alex.kovalyov@37fa36bf-7e54-0410-b8b4-67ef32aab22e>
2008-08-28 11:04:01 +0000

    Added merged sources

---
+++ tests/com/intridea/io/vfs/operations/acl/AclTest.java
@@ -0,0 +1,56 @@
+package com.intridea.io.vfs.operations.acl;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.intridea.io.vfs.operations.acl.Acl;
+
+@Test(groups="storage")
+public class AclTest {
+	public void testOperations () {
+		Acl acl = new Acl(null);
+		acl.denyAll();
+		
+		// Allow single right
+		acl.allow(Acl.Group.OWNER, Acl.Permission.WRITE);
+		Assert.assertTrue(acl.isAllowed(Acl.Group.OWNER, Acl.Permission.WRITE));
+		
+		// Deny single right
+		acl.deny(Acl.Group.OWNER, Acl.Permission.WRITE);
+		Assert.assertTrue(acl.isDenied(Acl.Group.OWNER, Acl.Permission.WRITE));
+		
+		// Allow rights
+		Acl.Permission[] rights = {Acl.Permission.READ, Acl.Permission.WRITE};
+		acl.allow(Acl.Group.AUTHORIZED, rights);
+		Assert.assertTrue(acl.isAllowed(Acl.Group.AUTHORIZED, Acl.Permission.WRITE));
+		Assert.assertTrue(acl.isAllowed(Acl.Group.AUTHORIZED, Acl.Permission.READ));
+		
+		// Deny rights
+		acl.deny(Acl.Group.AUTHORIZED, rights);
+		Assert.assertTrue(acl.isDenied(Acl.Group.AUTHORIZED, Acl.Permission.WRITE));
+		Assert.assertTrue(acl.isDenied(Acl.Group.AUTHORIZED, Acl.Permission.READ));
+		
+		// Allow all to group
+		acl.allow(Acl.Group.AUTHORIZED);
+		Assert.assertTrue(acl.isAllowed(Acl.Group.AUTHORIZED, Acl.Permission.WRITE));
+		Assert.assertTrue(acl.isAllowed(Acl.Group.AUTHORIZED, Acl.Permission.READ));
+		
+		// Deny all to group
+		acl.deny(Acl.Group.AUTHORIZED);
+		Assert.assertTrue(acl.isDenied(Acl.Group.AUTHORIZED, Acl.Permission.WRITE));
+		Assert.assertTrue(acl.isDenied(Acl.Group.AUTHORIZED, Acl.Permission.READ));
+		
+		// Allow all to all
+		acl.allowAll();
+		Assert.assertTrue(acl.isAllowed(Acl.Group.AUTHORIZED, Acl.Permission.WRITE));
+		Assert.assertTrue(acl.isAllowed(Acl.Group.EVERYONE, Acl.Permission.WRITE));
+		Assert.assertTrue(acl.isAllowed(Acl.Group.OWNER, Acl.Permission.WRITE));
+		
+		// Deny all to all
+		acl.denyAll();
+		Assert.assertTrue(acl.isDenied(Acl.Group.AUTHORIZED, Acl.Permission.WRITE));
+		Assert.assertTrue(acl.isDenied(Acl.Group.EVERYONE, Acl.Permission.WRITE));
+		Assert.assertTrue(acl.isDenied(Acl.Group.OWNER, Acl.Permission.WRITE));
+	}
+}
---
+++ tests/com/intridea/io/vfs/provider/s3/acl/PackageTest.java
@@ -0,0 +1,164 @@
+package com.intridea.io.vfs.provider.s3.acl;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.intridea.io.vfs.TestEnvirounment;
+import com.intridea.io.vfs.operations.acl.Acl;
+import com.intridea.io.vfs.operations.acl.IAclGetter;
+import com.intridea.io.vfs.operations.acl.IAclSetter;
+
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSystemException;
+import org.apache.commons.vfs.FileSystemManager;
+import org.apache.commons.vfs.VFS;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Test(groups={"storage", "acl"})
+public class PackageTest {
+
+	private FileSystemManager fsManager;
+	
+	private FileObject file;
+	
+	private Acl fileAcl;
+	
+	private String bucketName;
+	
+	@BeforeClass
+	public void setUp () throws FileNotFoundException, IOException {
+		Properties config = TestEnvirounment.getInstance().getConfig(); 
+		bucketName = config.getProperty("s3.testBucket", "vfs-s3-tests");
+		
+		fsManager = VFS.getManager();
+		file = fsManager.resolveFile("s3://" + bucketName + "/jonny.zip");
+	}
+	
+	@Test
+	public void getAcl () throws FileSystemException {
+		// Get ACL
+		IAclGetter aclGetter = (IAclGetter)file.getFileOperations().getOperation(IAclGetter.class);
+		aclGetter.process();
+		fileAcl = aclGetter.getAcl();
+		
+		// Owner can read/write
+		Assert.assertTrue(aclGetter.canRead(Acl.Group.OWNER));
+		Assert.assertTrue(aclGetter.canWrite(Acl.Group.OWNER));
+		
+		// Authorized coldn't read/write
+		Assert.assertFalse(aclGetter.canRead(Acl.Group.AUTHORIZED));
+		Assert.assertFalse(aclGetter.canWrite(Acl.Group.AUTHORIZED));
+		
+		// Guest also coldn't read/write
+		Assert.assertFalse(aclGetter.canRead(Acl.Group.EVERYONE));
+		Assert.assertFalse(aclGetter.canWrite(Acl.Group.EVERYONE));
+	}
+	
+	@Test(dependsOnMethods="getAcl")
+	public void setAcl () throws FileSystemException {
+		// Set allow read to Guest
+		fileAcl.allow(Acl.Group.EVERYONE, Acl.Permission.READ);		
+		IAclSetter aclSetter = (IAclSetter)file.getFileOperations().getOperation(IAclSetter.class);
+		aclSetter.setAcl(fileAcl);
+		aclSetter.process();
+		
+		// Verify
+		IAclGetter aclGetter = (IAclGetter)file.getFileOperations().getOperation(IAclGetter.class);
+		aclGetter.process();
+		Acl changedAcl = aclGetter.getAcl();
+		
+		// Guest can read
+		Assert.assertTrue(changedAcl.isAllowed(Acl.Group.EVERYONE, Acl.Permission.READ));
+		// Write rules for guest not changed
+		Assert.assertEquals(
+			changedAcl.isAllowed(Acl.Group.EVERYONE, Acl.Permission.WRITE), 
+			fileAcl.isAllowed(Acl.Group.EVERYONE, Acl.Permission.WRITE)
+		);
+		// Read rules not spreaded to another groups
+		Assert.assertEquals(
+			changedAcl.isAllowed(Acl.Group.AUTHORIZED, Acl.Permission.READ), 
+			fileAcl.isAllowed(Acl.Group.AUTHORIZED, Acl.Permission.READ)
+		);
+		Assert.assertEquals(
+			changedAcl.isAllowed(Acl.Group.OWNER, Acl.Permission.READ), 
+			fileAcl.isAllowed(Acl.Group.OWNER, Acl.Permission.READ)
+		);
+		
+		fileAcl = changedAcl;
+	}
+	
+	@Test(dependsOnMethods="setAcl")
+	public void setAcl2 () throws FileSystemException {
+		// Set allow all to Authorized
+		fileAcl.allow(Acl.Group.AUTHORIZED);		
+		IAclSetter aclSetter = (IAclSetter)file.getFileOperations().getOperation(IAclSetter.class);
+		aclSetter.setAcl(fileAcl);
+		aclSetter.process();
+
+		// Verify
+		IAclGetter aclGetter = (IAclGetter)file.getFileOperations().getOperation(IAclGetter.class);
+		aclGetter.process();
+		Acl changedAcl = aclGetter.getAcl();
+		
+		// Authorized can do everything
+		Assert.assertTrue(changedAcl.isAllowed(Acl.Group.AUTHORIZED, Acl.Permission.READ));
+		Assert.assertTrue(changedAcl.isAllowed(Acl.Group.AUTHORIZED, Acl.Permission.WRITE));
+		
+		// All other rules not changed
+		Assert.assertEquals(
+			changedAcl.isAllowed(Acl.Group.EVERYONE, Acl.Permission.READ), 
+			fileAcl.isAllowed(Acl.Group.EVERYONE, Acl.Permission.READ)
+		);
+		Assert.assertEquals(
+			changedAcl.isAllowed(Acl.Group.EVERYONE, Acl.Permission.WRITE), 
+			fileAcl.isAllowed(Acl.Group.EVERYONE, Acl.Permission.WRITE)
+		);
+		Assert.assertEquals(
+			changedAcl.isAllowed(Acl.Group.OWNER, Acl.Permission.READ), 
+			fileAcl.isAllowed(Acl.Group.OWNER, Acl.Permission.READ)
+		);
+		Assert.assertEquals(
+			changedAcl.isAllowed(Acl.Group.OWNER, Acl.Permission.WRITE), 
+			fileAcl.isAllowed(Acl.Group.OWNER, Acl.Permission.WRITE)
+		);
+		
+		fileAcl = changedAcl;
+	}
+	
+	@Test(dependsOnMethods={"setAcl2"})
+	public void setAcl3 () throws FileSystemException {
+		// Set deny to all
+		fileAcl.denyAll();		
+		IAclSetter aclSetter = (IAclSetter)file.getFileOperations().getOperation(IAclSetter.class);
+		aclSetter.setAcl(fileAcl);
+		aclSetter.process();
+		
+		// Verify
+		IAclGetter aclGetter = (IAclGetter)file.getFileOperations().getOperation(IAclGetter.class);
+		aclGetter.process();
+		Acl changedAcl = aclGetter.getAcl();
+		
+		Assert.assertTrue(changedAcl.isDenied(Acl.Group.OWNER, Acl.Permission.READ));
+		Assert.assertTrue(changedAcl.isDenied(Acl.Group.OWNER, Acl.Permission.WRITE));
+		Assert.assertTrue(changedAcl.isDenied(Acl.Group.AUTHORIZED, Acl.Permission.READ));
+		Assert.assertTrue(changedAcl.isDenied(Acl.Group.AUTHORIZED, Acl.Permission.WRITE));
+		Assert.assertTrue(changedAcl.isDenied(Acl.Group.EVERYONE, Acl.Permission.READ));
+		Assert.assertTrue(changedAcl.isDenied(Acl.Group.EVERYONE, Acl.Permission.WRITE));
+	}
+	
+	@AfterClass
+	public void restoreAcl () throws FileSystemException {
+		fileAcl.denyAll();
+		fileAcl.allow(Acl.Group.OWNER);
+		
+		// Set ACL
+		IAclSetter aclSetter = (IAclSetter)file.getFileOperations().getOperation(IAclSetter.class);
+		aclSetter.setAcl(fileAcl);
+		aclSetter.process();
+	}
+}
