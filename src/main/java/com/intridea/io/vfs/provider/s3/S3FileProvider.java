517874a40c52583fbf31415701b2e6f8805ecda1
abashev <abashev@gmail.com>
2010-10-29 19:25:02 +0400

    Remove dependency on log4j. Refresh code for new jets3t version

--- src/main/java/com/intridea/io/vfs/provider/s3/S3FileProvider.java
+++ src/main/java/com/intridea/io/vfs/provider/s3/S3FileProvider.java
@@ -1,5 +1,7 @@
 package com.intridea.io.vfs.provider.s3;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.commons.vfs.Capability;
 import org.apache.commons.vfs.FileName;
 import org.apache.commons.vfs.FileSystem;
@@ -8,7 +10,6 @@ import org.apache.commons.vfs.FileSystemOptions;
 import org.apache.commons.vfs.provider.AbstractOriginatingFileProvider;
 import org.apache.commons.vfs.util.UserAuthenticatorUtils;
 import org.apache.commons.vfs.UserAuthenticationData;
-import org.apache.log4j.Logger;
 import org.jets3t.service.S3Service;
 import org.jets3t.service.S3ServiceException;
 import org.jets3t.service.impl.rest.httpclient.RestS3Service;
@@ -21,7 +22,7 @@ import java.util.Collections;
 /**
  * An S3 file provider. Create an S3 file system out of an S3 file name. Also
  * defines the capabilities of the file system.
- * 
+ *
  * @author Marat Komarov
  * @author Matthias L. Jugel
  */
@@ -31,30 +32,30 @@ public class S3FileProvider extends AbstractOriginatingFileProvider {
 		Capability.CREATE,
 		Capability.DELETE,
 		// TODO: rename not supported by jets3t now
-		//Capability.RENAME, 
+		//Capability.RENAME,
 		Capability.GET_TYPE,
 		Capability.GET_LAST_MODIFIED,
 		Capability.SET_LAST_MODIFIED_FILE,
 		Capability.SET_LAST_MODIFIED_FOLDER,
-		Capability.LIST_CHILDREN, 
+		Capability.LIST_CHILDREN,
 		Capability.READ_CONTENT,
-		Capability.URI, 
+		Capability.URI,
 		Capability.WRITE_CONTENT
 		//Capability.APPEND_CONTENT
 	));
-	
+
 	/**
 	 * Auth data types necessary for AWS authentification.
 	 */
 	public final static UserAuthenticationData.Type[] AUTHENTICATOR_TYPES = new UserAuthenticationData.Type[] {
 		UserAuthenticationData.USERNAME, UserAuthenticationData.PASSWORD
-	};	
-	
+	};
+
 	/**
 	 * Default options for S3 file system.
 	 */
 	private static FileSystemOptions defaultOptions = new FileSystemOptions();
-	
+
 	/**
 	 * Returns default S3 file system options.
 	 * Use it to set AWS auth credentials.
@@ -63,7 +64,7 @@ public class S3FileProvider extends AbstractOriginatingFileProvider {
 	public static FileSystemOptions getDefaultFileSystemOptions () {
 		return defaultOptions;
 	}
-	
+
 	/**
 	 * S3 service instance
 	 */
@@ -72,8 +73,8 @@ public class S3FileProvider extends AbstractOriginatingFileProvider {
 	/**
 	 * Logger instance
 	 */
-	private Logger logger = Logger.getLogger(S3FileProvider.class);
-	
+	private Log logger = LogFactory.getLog(S3FileProvider.class);
+
 	public S3FileProvider() {
 		super();
 		setFileNameParser(S3FileNameParser.getInstance());
@@ -81,7 +82,7 @@ public class S3FileProvider extends AbstractOriginatingFileProvider {
 
 	/**
 	 * Create a file system with the S3 root provided.
-	 * 
+	 *
 	 * @param fileName the S3 file name that defines the root (bucket)
 	 * @param fileSystemOptions file system options
 	 * @return an S3 file system
@@ -89,33 +90,33 @@ public class S3FileProvider extends AbstractOriginatingFileProvider {
 	 */
 	protected FileSystem doCreateFileSystem(FileName fileName,
 			FileSystemOptions fileSystemOptions) throws FileSystemException {
-		
-		FileSystemOptions fsOptions = fileSystemOptions != null ? 
+
+		FileSystemOptions fsOptions = fileSystemOptions != null ?
 				fileSystemOptions : getDefaultFileSystemOptions();
-		
-		// Initialize once S3 service.		
+
+		// Initialize once S3 service.
 		if (service == null) {
 			UserAuthenticationData authData = null;
 			try {
 				// Read authData from file system options
 				authData = UserAuthenticatorUtils.authenticate(fsOptions, AUTHENTICATOR_TYPES);
-				
+
 				logger.info("Initialize Amazon S3 service client ...");
-				
+
 				// Fetch AWS key-id and secret key from authData
 				String keyId = UserAuthenticatorUtils.toString(UserAuthenticatorUtils.getData(authData, UserAuthenticationData.USERNAME, null));
-				String key = UserAuthenticatorUtils.toString(UserAuthenticatorUtils.getData(authData, UserAuthenticationData.PASSWORD, null)); 
+				String key = UserAuthenticatorUtils.toString(UserAuthenticatorUtils.getData(authData, UserAuthenticationData.PASSWORD, null));
 				if (keyId.length() + key.length() == 0) {
 					throw new FileSystemException("Empty AWS credentials");
 				}
-				
-				// Initialize S3 service client. 
+
+				// Initialize S3 service client.
 				AWSCredentials awsCredentials = new AWSCredentials(keyId, key);
 				service = new RestS3Service(awsCredentials);
 				logger.info("... Ok");
-				
+
 			} catch (S3ServiceException e) {
-				logger.error(String.format("Failed to initialize Amazon S3 service client. Reason: %s", 
+				logger.error(String.format("Failed to initialize Amazon S3 service client. Reason: %s",
 						e.getS3ErrorMessage()), e);
 				throw new FileSystemException(e);
 			}
@@ -123,14 +124,14 @@ public class S3FileProvider extends AbstractOriginatingFileProvider {
 				UserAuthenticatorUtils.cleanup(authData);
 			}
 		}
-		
+
 		// Construct S3 file system
 		return new S3FileSystem((S3FileName) fileName, service, fsOptions);
 	}
-	
+
 	/**
 	 * Get the capabilities of the file system provider.
-	 * 
+	 *
 	 * @return the file system capabilities
 	 */
 	public Collection<Capability> getCapabilities() {
--- src/main/java/com/intridea/io/vfs/provider/s3/S3FileSystem.java
+++ src/main/java/com/intridea/io/vfs/provider/s3/S3FileSystem.java
@@ -1,22 +1,21 @@
 package com.intridea.io.vfs.provider.s3;
 
+import java.util.Collection;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.commons.vfs.FileName;
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemException;
 import org.apache.commons.vfs.FileSystemOptions;
 import org.apache.commons.vfs.provider.AbstractFileSystem;
-import org.apache.log4j.Logger;
-
 import org.jets3t.service.S3Service;
-import org.jets3t.service.S3ServiceException;
+import org.jets3t.service.ServiceException;
 import org.jets3t.service.model.S3Bucket;
-
-
-import java.util.Collection;
 
 /**
  * An S3 file system.
- * 
+ *
  * @author Marat Komarov
  * @author Matthias L. Jugel
  */
@@ -24,8 +23,8 @@ public class S3FileSystem extends AbstractFileSystem {
 	private S3Service service;
 	private S3Bucket bucket;
 
-	private Logger logger = Logger.getLogger(S3FileSystem.class);
-	
+	private Log logger = LogFactory.getLog(S3FileSystem.class);
+
 	public S3FileSystem(S3FileName fileName, S3Service service,
 			FileSystemOptions fileSystemOptions) throws FileSystemException {
 		super(fileName, null, fileSystemOptions);
@@ -37,16 +36,17 @@ public class S3FileSystem extends AbstractFileSystem {
 				bucket = service.createBucket(bucketId);
 			}
 			logger.info(String.format("Created new S3 FileSystem %s", bucketId));
-		} catch (S3ServiceException e) {
-			String s3message = e.getS3ErrorMessage();
+		} catch (ServiceException e) {
+			String s3message = e.getErrorMessage();
+
 			if (s3message != null) {
-				throw new FileSystemException(s3message);	
+				throw new FileSystemException(s3message);
 			} else {
 				throw new FileSystemException(e);
 			}
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	protected void addCapabilities(Collection caps) {
 		caps.addAll(S3FileProvider.capabilities);
